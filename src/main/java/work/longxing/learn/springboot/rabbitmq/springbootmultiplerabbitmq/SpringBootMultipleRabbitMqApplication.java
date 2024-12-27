b587e9324e2746c5872efe5022ce02f2a1ba4ee2
mxuexxmy <1989718921@qq.com>
2024-12-27 23:49:01 +0800

    feat: project initial

---
+++ src/main/java/work/longxing/learn/springboot/rabbitmq/springbootmultiplerabbitmq/SpringBootMultipleRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package work.longxing.learn.springboot.rabbitmq.springbootmultiplerabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/longxing/learn/springboot/rabbitmq/springbootmultiplerabbitmq/SpringBootMultipleRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package work.longxing.learn.springboot.rabbitmq.springbootmultiplerabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultipleRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
